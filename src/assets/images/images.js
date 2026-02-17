9a6e4ef07e727163290ec406849f8ebc224ef78e
Helder <elder.goma@gmail.com>
2026-02-17 21:01:43 +0300

    create mock data for games and categories.

---
+++ src/assets/images/images.js
@@ -0,0 +1,19 @@
+import game1 from "./GatesOfOlympus1000 1.png";
+import game2 from "./HotFiesta 1.png";
+import game3 from "./StarlightPrincess1000 1.png";
+import game4 from "./TheDogHouse_new 1.png";
+import game5 from "./TheWildos2.png";
+import game6 from "./ZeusvsHades_GodsOfWar 1.png";
+import game7 from "./image 878.png";
+import game8 from "./image 881.png";
+import game9 from "./image 927.png";
+import game10 from "./image 928.png";
+import game11 from "./image 929.png";
+import game12 from "./image 930.png";
+import game13 from "./image 931.png";
+import game14 from "./image 933.png";
+import game15 from "./image 934.png";
+import pragmaticPlay from "./PragmaticPlay_new.png";
+
+export { game1, game2, game3, game4, game5, game6, game7, game8, game9, game10, game11, game12, game13, game14, game15, pragmaticPlay };
+
---
+++ src/data.js
@@ -0,0 +1,27 @@
+import { game1, game2, game3, game4, game5, game6, game7, game8, game9, game10, game11, game12, game13, game14, game15 } from './assets/images/images.js';
+export const CATEGORIES = [
+  { id: 'all', label: 'Все' },
+  { id: 'fast', label: 'Быстрые Игры' },
+  { id: 'slots', label: 'Слоты' },
+  { id: 'live', label: 'Live Казино' },
+  { id: 'popular', label: 'Популярные' },
+  { id: 'new', label: 'Новые' }
+];
+
+export const INITIAL_GAMES = [
+  { id: 1, title: 'Gates of Olympus', category: 'popular', img: game1 },
+  { id: 2, title: 'Alien Turtles', category: 'new', img: game2 },
+  { id: 3, title: 'Wild Skyfire', category: 'slots', img: game3 },
+  { id: 4, title: 'Batman Crash', category: 'fast', img: game4 },
+  { id: 5, title: 'Sweet Bonanza', category: 'popular', img: game5 },
+  { id: 6, title: 'Sugar Rush', category: 'slots', img: game6 },
+  { id: 7, title: 'Creepy Circus', category: 'new', img: game7 },
+  { id: 8, title: 'Aviator', category: 'fast', img: game8 },
+  { id: 9, title: 'Live Roulette', category: 'live', img: game9 },
+  { id: 10, title: 'Blackjack', category: 'live', img: game10 },
+  { id: 11, title: 'Poker', category: 'live', img: game11 },
+  { id: 12, title: 'Roulette', category: 'live', img: game12 },
+  { id: 13, title: 'Baccarat', category: 'live', img: game13 },
+  { id: 14, title: 'Craps', category: 'live', img: game14 },
+  { id: 15, title: 'Live Blackjack', category: 'live', img: game15 },
+];
