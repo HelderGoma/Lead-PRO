94f3bbc428d0f056f8423b646a6db20980314095
Helder <elder.goma@gmail.com>
2026-02-17 21:02:46 +0300

    implement GameCard with hover effects and play button

---
+++ src/components/Games/GameCard.jsx
@@ -0,0 +1,19 @@
+import styles from './GameCard.module.css';
+import { pragmaticPlay } from '../../assets/images/images';
+
+export default function GameCard({ game }) {
+    return (
+        <div className={styles.card}>
+            <div className={styles.imageBox}>
+                <img src={game.img} className={styles.img} alt={game.title} />
+                <div className={styles.playOverlay}>
+                    <div className={styles.playIcon}>▶</div>
+                </div>
+            </div>
+            <div className={styles.body}>
+                <h4 className={styles.name}>{game.title}<a href="#"><img src={pragmaticPlay} alt="" /></a></h4>
+                <p className={styles.vendor}></p>
+            </div>
+        </div>
+    );
+}
